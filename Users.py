c985c745487342ded09e5bb42786ce941c413cef
MrOmarA <mrbrooklynwolf@gmail.com>
2023-05-01 10:58:29 -0400

    Update Users.py

--- Users.py
+++ Users.py
@@ -1,16 +1,24 @@
 import uuid 
+import time
 
 class Basic:
     def __init__(self, id, tag):
         self.id = id
         self.name = tag
         
-class fighterpilot(Basic):
+class fighterjet(Basic):
     def __init__(self,id,tag,jet):
         super().__init__(self,id,tag)
         self.jet = jet
     def __str__(self):
         return f"{self.name}, {self.jet}, {self.id}, {self.tag}"
+
+print("choose your fighter")
+time.sleep(1)
+
+#first choice
+
+choice1 = input
 
 
 
